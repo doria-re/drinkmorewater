2d8b2019ed9cfe47b1d748db2869ee9676130516
doria <resonanta@naver.com>
2022-01-26 14:44:28 +0900

    when click change unit button, amount go back to 0

--- js/unit.js
+++ js/unit.js
@@ -4,6 +4,7 @@ const clickUnitButton = document.querySelector("#change-unit");
 
 
 function changeUnit(){
+    waterAmount.innerText = 0;
     if (unit[0].innerText.includes("컵")){
         for (let i=0; i<unit.length; i++){
             unit[i].innerText = unit[i].innerText.replace("컵", "ml");
